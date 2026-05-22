acc76d5b1754e7abe1793d9a31931cecb123f6d2
NexoZegknost <huuducnguyen2007@gmail.com>
2026-05-22 14:29:20 +0700

    Project specification

---
+++ RISC CPU.srcs/sources_1/new/UX.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.05.2026 14:26:59
+// Design Name: 
+// Module Name: UX
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module UX(
+
+    );
+endmodule
